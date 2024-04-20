145b2d37d42818d7964bee871c514b0f8c5254c2
hugo_coisne <coisnehugo@gmail.com>
2024-04-20 15:20:53 +0200

    improve receipe entity

--- src/main/java/entity/Receipe.java
+++ src/main/java/entity/Receipe.java
@@ -3,10 +3,10 @@ package entity;
 import java.util.List;
 
 public class Receipe {
-    int id;
-    String name;
-    String description;
-    List<Item> ingredients;
+    private int id;
+    private String name;
+    private String description;
+    private List<Item> ingredients;
     public Receipe(int id, String name, String description, List<Item> ingredients){
         this.id = id;
         this.name = name;
@@ -14,13 +14,44 @@ public class Receipe {
         this.ingredients = ingredients;
     }
 
-    @Override
-    public String toString() {
-        return "Receipe(" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", ingredients=" + ingredients +
-                ')';
+    public String getDescription() {
+        return description;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Item> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Item> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void addIngredient(Item ingredient){
+        this.ingredients.add(ingredient);
+    }
+
+    public void removeIngredient(Item ingredient){
+        this.ingredients.remove(ingredient);
+    }
+
 }
